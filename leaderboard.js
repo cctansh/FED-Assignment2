74850265796b55b172089ebd3e45e79835c58933
cctansh <s10260078@connect.np.edu.sg>
2024-02-02 20:43:56 +0800

    added leaderboard loading

--- leaderboard.js
+++ leaderboard.js
@@ -1,42 +1,68 @@
-var data = [
-    {
-        dn: 'name1',
-        spent: '23.33'
-    },
-    {
-        dn: 'name2',
-        spent: '24.33'
-    },
-    {
-        dn: 'name3',
-        spent: '25.33'
-    },
-    {
-        dn: 'name4',
-        spent: '27.33'
-    }
-];
+const apiKey = "65b665611aac406df1278a6f";
+const apiUrl = 'https://products-aa44.restdb.io/rest/leaderboard';
 
+let loading = document.getElementById("loading");
+let loadingIcon = document.getElementById("loading-icon");
+let nav = document.getElementById("navbar");
+let body = document.getElementById('content')
+let foot = document.getElementById('foot')
+
+var usersData = [];
 let board = document.getElementById('leaderboard');
+
+loading.classList.remove('hidden');
+loadingIcon.classList.remove('hidden');
+nav.classList.add('hidden');
+body.classList.add('hidden');
+foot.classList.add('hidden');
+
+fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-apikey': apiKey,
+      "Cache-Control": "no-cache"
+    },
+})
+    .then(response => response.json())
+    .then(response => {
+        for (var i = 0; i < response.length; i++) {
+            usersData.push({
+                apiID: response[i]._id,
+                dn: response[i].dn,
+                email: response[i].email,
+                password: response[i].password,
+                spent: response[i].spent,
+            })
+        }
+        console.log(usersData);
+        printLeaderboard();
+        loading.classList.add('hidden');
+        loadingIcon.classList.add('hidden');
+        nav.classList.remove('hidden');
+        body.classList.remove('hidden');
+        foot.classList.remove('hidden');
+})
+    .catch(error => {
+      console.error('Error:', error);
+});
 
 function printLeaderboard() {
     let text = '';
-    for (var i = 1; i <= data.length; i++) {
+    for (var i = 1; i <= usersData.length; i++) {
         text = `${text}
             <div class="boardRow row${i}">
                 <div class="rank">
                     ${i}
                 </div>
                 <div class="name">
-                    ${data[i-1].dn}
+                    ${usersData[i-1].dn}
                 </div>
                 <div class="spent">
-                    $${data[i-1].spent}
+                    $${usersData[i-1].spent}
                 </div>
             </div>
             `
     }
     board.innerHTML = text;
 }
-
-printLeaderboard();
